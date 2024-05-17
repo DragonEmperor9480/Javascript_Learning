c05173ec8acde2b31b6256166fdae3877a07677e
DragonEmperor9480 <amruteshnaregal1234@gmail.com>
2024-05-18 01:55:17 +0530

    Variables

---
+++ variables.js
@@ -0,0 +1,11 @@
+var Name;
+Name = "Amrut";
+console.log(Name);
+
+let text; //creating a variable or declaring a variable
+text = "Github";//Assigning a value to variable
+console.log(text);
+
+const age=20;
+
+console.log(age);
