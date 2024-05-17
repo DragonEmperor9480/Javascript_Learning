032ba188a552c1cb6c7490e728a128eed15d2a27
DragonEmperor9480 <amruteshnaregal1234@gmail.com>
2024-05-17 20:31:44 +0530

    Difference between var,let andconst keyword

---
+++ var_let_message.js
@@ -0,0 +1,21 @@
+//var keyword
+var name = "Ayato";
+var name = "Ayaka";
+var name = "Kamisato";
+console.log(name);
+
+//As shown above we can use "var" keyword many times as i want.
+
+//let Keyword
+let fname = "Zhongli";
+//let fname = "Childe";
+fname = "Childe";
+console.log(fname);
+
+//Here we cannot use "let" keyword again and again but we can reuse the variable to assign a different value
+
+//const keyword
+const number = "one";
+//const number = "one";
+//number = "one";
+console.log(number);
