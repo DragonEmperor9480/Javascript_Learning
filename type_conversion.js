2217c471994d839c6c7d999745e21cd8a998c3b5
DragonEmperor9480 <amruteshnaregal1234@gmail.com>
2024-05-18 18:24:09 +0530

    type conversion

---
+++ type_conversion.js
@@ -0,0 +1,25 @@
+const a = "10"
+const b = "20"
+
+//converting string to number
+const c = Number(a)
+const d = Number(b)
+
+console.log(typeof a)
+console.log(typeof b)
+console.log(typeof c)
+console.log(typeof d)
+
+//converting number to string
+const e = String(d)
+console.log(typeof e)
+
+//Boolean
+//If a variable has any text the output will be always true and it will be false if string is empty
+//If the variable contains any number it will always be true except if it contains zero then the boolean value is false
+
+console.log(Boolean(a))
+console.log(Boolean(d))
+
+
+
