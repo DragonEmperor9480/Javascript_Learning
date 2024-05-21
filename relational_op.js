a67f7ec0aea8a9cf3e845c3bc5c53a52a3af3819
DragonEmperor9480 <amruteshnaregal1234@gmail.com>
2024-05-21 19:46:27 +0530

    Relational Operators

---
+++ relational_op.js
@@ -0,0 +1,26 @@
+var a = 5;
+var b = 10;
+var c = 5;
+
+// 1. Greater than operator: '>'
+console.log("Greater than Operator")
+console.log(a>b)
+console.log(a>c)
+
+console.log()
+//2. Greater than or equal to operator
+console.log("Greater than or equal to Operator")
+console.log(a>=b)
+console.log(a>=c)
+
+console.log()
+//3.Less than operator
+console.log("Less than operator")
+console.log(a<b)
+console.log(a<c)
+
+console.log()
+//4.Less than or equal to operator
+console.log("Less than or equal to operator")
+console.log(a<=b)
+console.log(a<=c)
