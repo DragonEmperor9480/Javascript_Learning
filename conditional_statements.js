db6e366a749f25d3b7c6efd71bb96c063a4a28fa
DragonEmperor9480 <amruteshnaregal1234@gmail.com>
2024-05-21 20:31:04 +0530

    conditional statements

---
+++ conditional_statements.js
@@ -0,0 +1,63 @@
+//Conditional Statements
+// 1. if
+// 2. if-else
+// 3. nested-if
+// 4. if-else-if ladder
+
+console.log("If condition")
+
+if (10>6)
+    {
+        console.log("If condition executed!");
+    }
+
+console.log()
+
+console.log("If else condition")
+if(5>5)
+    {
+        console.log("If condition executed!")
+    }
+else
+    {
+        console.log("Else condition elecuted")
+    }
+
+console.log()
+console.log("nested-if")
+
+var a = 5;
+if (a > 5)
+    {
+        console.log("First Condition");
+    }
+if (a > 5)
+    {
+        console.log("Second condition");
+    }
+if ( a==5)
+    {
+        console.log("Third Condition");
+    }
+
+console.log()
+
+console.log("if-else-if ladder")
+
+if (a > 5)
+    {
+        console.log("First Condition");
+    }
+else if (a > 5)
+    {
+        console.log("Second condition");
+    }
+else if ( a!=5)
+    {
+        console.log("Third Condition");
+    }
+else
+    {
+        console.log("fourth condition")
+    }
+
