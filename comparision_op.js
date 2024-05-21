118e4484e409667a9452cfe42a63b632d030c793
DragonEmperor9480 <amruteshnaregal1234@gmail.com>
2024-05-21 19:39:03 +0530

    Comparision Operators

---
+++ comparision_op.js
@@ -0,0 +1,48 @@
+var a = 5;
+var b = 10;
+var c = 5;
+
+
+// Equality Operator: '=='
+console.log("Equality Operator");
+
+console.log(a==a);
+console.log(a==b);
+console.log(a==c);
+
+console.log(0 == false);
+console.log(1 == true);
+
+console.log();
+
+//Inequality Operator: '!='
+console.log("InEquality Operator");
+
+console.log(a!=b);
+console.log(a!=c);
+
+console.log(0 != false);
+console.log(1 != true);
+
+console.log();
+
+
+//Strict Equality Operator
+// for strict equality operator both the variables should be same value and must have same datatype
+console.log("Strict Equality Operator");
+
+console.log(5 === '5');
+console.log(5 === 5);
+console.log(0 === false);
+
+console.log()
+//Strict InEquality Operator
+//this operator is used to compare the inequality of two operands with type. if bothvalue and type are not equal then
+// the condition will be true otherwise false
+console.log("Strict Inequality Operator")
+
+console.log(5 !== '5');
+console.log(5 !== 5);
+console.log(0 !== false);
+
+
