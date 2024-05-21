28023ed570ee7a377e296549df688519420b0de8
DragonEmperor9480 <amruteshnaregal1234@gmail.com>
2024-05-21 21:07:15 +0530

    Ternary Operator

---
+++ ternary_op.js
@@ -0,0 +1,8 @@
+//Ternary Operator
+
+//Syntax:
+//      condition ? value if true : value if false
+
+let age = 60;
+let result = (age>59)? "Senior citizen":"Not a Senior Citizen";
+console.log(result)
