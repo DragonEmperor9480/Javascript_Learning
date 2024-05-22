c4a4e80ad960b0e23e5039ba5bc3d4fe591f1b43
DragonEmperor9480 <amruteshnaregal1234@gmail.com>
2024-05-22 10:45:08 +0530

    Logical Operators

---
+++ logical_op.js
@@ -0,0 +1,15 @@
+//Logical Operators
+//There are three logical Operators
+/*
+ *  - ! : NOT
+    - && : AND
+    - || : OR
+ */
+
+    let a = 5;
+    let b = 10;
+    console.log(!a);
+
+    console.log((a>b) && (b>a))
+
+    console.log((a>b) || (b>a))
