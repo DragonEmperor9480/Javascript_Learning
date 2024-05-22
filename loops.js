2b2bc7e38388918ead9a7a696df72df5078d8896
DragonEmperor9480 <amruteshnaregal1234@gmail.com>
2024-05-22 11:44:33 +0530

    Loops

---
+++ loops.js
@@ -0,0 +1,33 @@
+/**
+ * Entry controlled loops:
+ * 1.For Loop
+ * 2. While loop
+ * 
+ * Exit control loops
+ * 1.Do while loop
+ * 
+ */
+console.log("For Loop")
+
+for (i=0;i<10;i++)
+    {
+        console.log(i)
+    }
+console.log()
+
+console.log("While Loop")
+var a = 0
+while(a<10)
+    {
+        console.log(a)
+        a++
+    }
+
+console.log()
+console.log("Do while loop")
+
+var j = 0
+do{
+    console.log(j)
+    j++
+}while(j<10)
