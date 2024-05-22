5fb6a3331b21f90f6e249ecbe0f770fc18450e22
DragonEmperor9480 <amruteshnaregal1234@gmail.com>
2024-05-22 21:21:10 +0530

    Function Declearation methods

---
+++ function_decleration.js
@@ -0,0 +1,23 @@
+/**
+ * Functions
+ */
+
+function add()
+{
+    console.log(5+5);
+}
+
+function sub(a, b)
+{
+    console.log(a-b);
+}
+
+function div(a, b)
+{
+    return (a/b);
+}
+
+add()
+sub(5,5)
+console.log(div(5,5))
+
