36457ae46a20c4c2219992100a042b9f9934fdf1
DragonEmperor9480 <amruteshnaregal1234@gmail.com>
2024-06-03 07:38:39 +0530

    practice: Basic Array uses

---
+++ Pratice/array_pratice.js
@@ -0,0 +1,13 @@
+let fruits = ["Apple", "Banana","Orange"];
+
+console.log(typeof fruits);
+console.log(fruits);
+console.log(fruits[1]);
+
+fruits.push("Cherry");
+console.log(fruits);
+
+fruits.shift();
+console.log(fruits);
+
+
