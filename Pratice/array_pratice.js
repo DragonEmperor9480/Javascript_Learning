c858eaf13c35a43c3589608bad88eab2fa63db66
DragonEmperor9480 <amruteshnaregal1234@gmail.com>
2024-06-04 12:19:16 +0530

    practice: More about arrays

--- Pratice/array_pratice.js
+++ Pratice/array_pratice.js
@@ -1,13 +1,27 @@
+//creating array
 let fruits = ["Apple", "Banana","Orange"];
 
 console.log(typeof fruits);
+
+//printing the array contents
 console.log(fruits);
+
+//printing array elements from a specific index
 console.log(fruits[1]);
 
+//Appending a new value to array
 fruits.push("Cherry");
 console.log(fruits);
 
+//dropping the first element 
 fruits.shift();
 console.log(fruits);
+
+//to print every element present in array with the help of loop
+fruits.forEach(element =>{
+    console.log(element)
+
+});
+
 
 
