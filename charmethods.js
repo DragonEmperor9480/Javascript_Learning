0572d0dd24656b41bac0fb0a09da5b355e28d025
DragonEmperor9480 <amruteshnaregal1234@gmail.com>
2024-06-06 22:41:14 +0530

    Strings: char methods

---
+++ charmethods.js
@@ -0,0 +1,21 @@
+let laptop = "ASUS ROG STRIX";
+console.log(laptop.charAt(0));
+console.log(laptop.charAt(1));
+console.log(laptop.charAt(2));
+console.log(laptop.charAt(3));
+
+// This function returns a single character located at the index specified as the argument to the function. 
+//If the index is out of range, then this function returns an empty string. 
+
+/**
+ * charCodeAt() method returns a Unicode character set code unit of the character present at the index in the string specified as the argument.
+ *  The syntax of the method is as follows:
+ * str.charCodeAt(index)
+ */
+
+console.log()
+console.log(laptop.charCodeAt(0));
+
+//If you use an index value which is out of range, the output will be NaN
+
+
