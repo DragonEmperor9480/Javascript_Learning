1ddaa70aa3bf8cccd04356120773b8effbd6087f
DragonEmperor9480 <amruteshnaregal1234@gmail.com>
2024-06-06 22:41:33 +0530

    Strings: upper case and lower case methods

---
+++ upper_lower_case.js
@@ -0,0 +1,7 @@
+let full_name = "Akshay Sagar"
+
+console.log(full_name.toUpperCase());
+console.log(full_name.toLowerCase());
+
+console.log(full_name.toLocaleUpperCase());
+console.log(full_name.toLocaleLowerCase());
