ada36ffc9946cc3f56d470a594dc66bab53b0962
DragonEmperor9480 <amruteshnaregal1234@gmail.com>
2024-06-06 22:40:56 +0530

    Strings: indexOf method

---
+++ indexOfmethod.js
@@ -0,0 +1,8 @@
+//str.indexOf() function finds the index of the first occurrence of the argument string in the given string. 
+//The value returned is 0-based. The syntax of the function is as follows: 
+
+let dept = "Computer Science and Engineering";
+console.log(dept.indexOf("Science"));
+
+//The output will be -1 as this method is case sensitive
+console.log(dept.indexOf("science"));
