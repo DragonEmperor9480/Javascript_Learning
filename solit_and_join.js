8aeb8f1c21e494f2c9a24d0343ace976c2037081
DragonEmperor9480 <amruteshnaregal1234@gmail.com>
2024-06-12 15:13:09 +0530

    Arrays: split and join

---
+++ solit_and_join.js
@@ -0,0 +1,7 @@
+var sentence = "I love Programming"
+console.log(sentence.split(" ",2));
+
+var fruits = ["Apple","Banana","Chickoo","Guava","Papaya"]
+console.log(fruits)
+
+console.log(`the fruits are ${fruits.join(",")}`)
