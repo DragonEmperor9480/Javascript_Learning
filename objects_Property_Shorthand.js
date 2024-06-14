67c18cf8a973bdce27312834409e15e5e7d973c2
DragonEmperor9480 <amruteshnaregal1234@gmail.com>
2024-06-14 10:31:23 +0530

    Objects: Property Shorthand

---
+++ objects_Property_Shorthand.js
@@ -0,0 +1,28 @@
+// // Objects in JavaScript are the most important data-type and forms the building blocks for modern JavaScript.
+//  These objects are quite different from JavaScript primitive data-types (Number, String, Boolean, null, undefined, and symbol) 
+//  in the sense that while these primitive data-types all store a single value each (depending on their types).
+
+// // The shorthand syntax for object property value is very popular and widely used nowadays. 
+// The code looks cleaner and easy to read. The shorthand property makes the code size smaller and simpler.
+
+const name = "Amrut";
+const college = "SKSVMACET";
+const yop = 2025;
+
+//method 1
+const details = {
+    name,
+    college,
+    yop
+};
+
+console.log(details);
+
+//method 2
+const details2 = {
+    name,
+    college,
+    yop
+};
+
+console.log(details2);
